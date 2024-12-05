3d558087331ca7731d208b7ec178133c00591d86
luquinhas do amassa <152299836+lucaspellanda@users.noreply.github.com>
2024-12-05 16:00:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de função para aumentar a interatividade (apenas um exemplo simples)
+document.querySelector('footer').addEventListener('click', function() {
+    alert("Obrigado por visitar o site!");
+});
